04e4fa9b6dafa993a759c508e6ed59fd26760a83
cesarolvr <cesar2012oliveira@gmail.com>
2023-10-12 18:52:24 -0300

    testing pub sub events

--- index.js
+++ index.js
@@ -2,21 +2,38 @@ import express from "express";
 import { createServer } from "node:http";
 import { Server } from "socket.io";
 
+// // Database
+// import { createClient } from 'redis';
+// const createRoom = async () => {
+//   const client = createClient();
+//   client.on('error', err => console.log('Redis Client Error', err));
+//   await client.connect();
+//   await client.set('active_rooms', 'value');
+// }
+
+
 const app = express();
 const server = createServer(app);
-const io = new Server(server, {
+const socket = new Server(server, {
   cors: {
     origin: "http://localhost:8000",
     methods: ["GET", "POST", "HEAD"],
   },
 });
 
-app.get("/", (req, res) => {
-  res.sendFile(new URL("./index.html", import.meta.url).pathname);
-});
+socket.on("connection", (e) => {
+  const clientId = e.id
+  socket.emit("connection_status", { connected: true });
 
-io.on("connection", (socket) => {
-  console.log("a user connected");
+  socket.on("create_room", (event) => {
+    console.log('aaaaaabbb', event)
+    // socket.join(event.name)
+    // socket.to(clientId).emit("room_created", event.name);
+  });
+
+  socket.on("disconnect", () => {
+    socket.emit("connection_status", { connected: false });
+  });
 });
 
 server.listen(3000, () => {
