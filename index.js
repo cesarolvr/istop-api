df612a590eec7bfb648e1c5c6b2836bf8f4731aa
Cesar Faber <cesarolvr@Cesars-MacBook-Air.local>
2024-01-13 15:39:45 -0300

    saving before pivoting

--- index.js
+++ index.js
@@ -2,28 +2,79 @@ import express from "express";
 import { createServer } from "node:http";
 import cors from "cors";
 import { Server } from "socket.io";
+import { createAdapter } from "@socket.io/redis-adapter";
+import { createClient } from "redis";
+import { v4 as uuidv4 } from "uuid";
+import {InMemorySessionStore} from "./src/sessionStore"
+
+const FRONT_URL = ["http://localhost:8000"];
 
 const app = express();
 app.use(cors());
 const server = createServer(app);
 
+//https://socket.io/docs/v4/using-multiple-nodes/#enabling-sticky-session
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:8000",
-    methods: ["GET", "POST", "HEAD"],
+    origin: FRONT_URL,
+    methods: ["GET", "POST"],
+    transports: ["websocket", "polling"],
+    credentials: true,
   },
+});
+
+const url = "redis://127.0.0.1:6379";
+const pubClient = createClient({ url });
+const subClient = pubClient.duplicate();
+
+const initPubSub = async () => {
+  await Promise.all([pubClient.connect(), subClient.connect()]);
+  io.adapter(createAdapter(pubClient, subClient));
+};
+
+initPubSub();
+
+const sessionStore = new InMemorySessionStore();
+
+io.use((socket, next) => {
+  const sessionID = socket.handshake.auth.sessionID;
+  if (sessionID) {
+    // find existing session
+    const session = sessionStore.findSession(sessionID);
+    if (session) {
+      socket.sessionID = sessionID;
+      socket.userID = session.userID;
+      socket.username = session.username;
+    }
+    return next();
+  }
+  // const username = socket.handshake.auth.username;
+  // // if (!username) {
+  // //   return next();
+  // // }
+  // create new session
+  socket.sessionID = uuidv4();
+  socket.userID = uuidv4();
+  socket.username = username;
+  return next();
 });
 
 io.on("connection", (socket) => {
   const clientId = socket.id;
   socket.emit("connection_status", { connected: true });
 
+  socket.emit("auth", {
+    sessionID: socket.sessionID,
+    userID: socket.userID,
+  });
+
   socket.on("create_room", (event) => {
-    socket.join(event.name);
     io.to(clientId).emit("room_created", event.name);
   });
 
   socket.on("request_to_join", ({ auth, roomName }) => {
+    // const roomExists = io.of("/").adapter.rooms.has(roomName)
+    // console.log('roomExists', roomExists, roomName)
     socket.join(roomName);
     io.to(roomName).emit("player_joined", { auth, roomName });
   });
@@ -33,18 +84,6 @@ io.on("connection", (socket) => {
   });
 });
 
-const getActiveRooms = () => {
-  const rooms = io.sockets.adapter.rooms
-  console.log(io.sockets.adapter.rooms)
-  const roomsList = Object.keys(rooms)
-  return  roomsList;
-};
-
-app.get("/active-rooms", (req, res) => {
-  const rooms = getActiveRooms();
-  res.json(rooms);
-});
-
 server.listen(3000, () => {
-  console.log("server running at http://localhost:3000");
+  console.log(`server running at http://localhost at port 3000`);
 });
